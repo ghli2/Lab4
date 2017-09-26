ddaf4891c1517e9d17596fb7a556559ebe534501
George <ghli2@illinois.edu>
2017-09-26 12:49:15 -0500

    did things

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,34 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
-         */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+/**
+ * Does things.
+ */
+public class ReclamationProject {
+    /**
+     *
+     * @param af a string
+     * @param bf another string
+     * @return the largest and first shared substring of a and b
+     */
+    static String doit(final String af, final String bf) {
+        String a = af;
+        String b = bf;
+        if (a.length() > b.length()) {
+            String c = a;
+            a = b;
+            b = c;
+        }
+        String r = "";
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() -  j; k++) {
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    }
+                }
+            }
+        }
+        return r;
+    }
 }
